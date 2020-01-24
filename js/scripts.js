c0ef5cfd2c3566e96c20f8fe04aea7a756c5d2aa
jason <dicipleofrollins@gmail.com>
2020-01-24 15:25:40 -0800

    add if/if else statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,19 @@
 // business logic 
 var beeparr = [];
 
-var boop = function(result){
-
-}
+for  (var i=0; i<=input; i++){
+  if (i.toString().includes(1) === true ){
+    beeparr.push("beep!");
+  }
+  else if (i.toString().includes(2) === true ){
+    beeparr.push("boop!"); 
+  }
+  else if(i.toString().includes(3) === true ){
+     beeparr.push("I can't help you Dave.");
+   }
+  else {
+    beeparr.push(i);
+  }}
 
 // UI logic
 $(document).ready(function() {
@@ -13,9 +23,7 @@ $(document).ready(function() {
     var count = beeparr.pop();
     beeparr.splice(0,count);
     var input = $('#input').val();
-    for  (var i=0; i<=input; i++){
-      beeparr.push(i);
-    }
+
    $("#answer").text(beeparr);
   
   });
