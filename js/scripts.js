ede3689e69a6b98180ef9f880a36e71cd79ac80d
jason <dicipleofrollins@gmail.com>
2020-01-24 14:59:21 -0800

    add scripts logic to fis read out display

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,22 @@
 // business logic 
 var beeparr = [];
 
+var boop = function(result){
+
+}
+
 // UI logic
 $(document).ready(function() {
   $("#beep").submit(function(event) {
     event.preventDefault();
-   
+    
+    var count = beeparr.pop();
+    beeparr.splice(0,count);
     var input = $('#input').val();
     for  (var i=0; i<=input; i++){
       beeparr.push(i);
     }
-    alert(beeparr);
+   $("#answer").text(beeparr);
+  
   });
 });
